5821271b2bd74e980c3d3c95ab3f168b6c6291ad
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-12-07 10:09:25 +0100

    Try ESM import

--- grammar.js
+++ grammar.js
@@ -7,14 +7,14 @@
 /* eslint-disable-next-line spaced-comment */
 /// <reference types="tree-sitter-cli/dsl" />
 
-const HTML = require('tree-sitter-html/grammar');
+import HTML from 'tree-sitter-html/grammar';
 
 const PREC = {
   CALL: 1,
   ALIAS: 2,
 };
 
-module.exports = grammar(HTML, {
+export default grammar(HTML, {
   name: 'angular',
 
   externals: ($, original) =>
