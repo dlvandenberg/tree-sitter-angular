491a48d697ba10c7cba010ecd3b49870c76a1b59
Dennis van den Berg <dennis.vandenberg@nedap.com>
2024-04-29 14:51:00 +0200

    chore: some git config fixes

---
+++ bindings/c/tree-sitter-angular.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_ANGULAR_H_
+#define TREE_SITTER_ANGULAR_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_angular(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_ANGULAR_H_
